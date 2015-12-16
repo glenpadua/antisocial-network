e099fd55c6b8199df802df2b3b20503bdc216b9f
Glen Padua <glenpadua01@gmail.com>
2015-12-16 23:23:04 +0530

    Added functions in controller to create a post and like a post

--- public/app/controllers/postCtrl.js
+++ public/app/controllers/postCtrl.js
@@ -1,6 +1,6 @@
 angular.module('postCtrl', ['postService'])
 
-	.controller('postController', function(Post) {
+	.controller('postController', function(Post, $route) {
 	
 		var vm = this;
 	
@@ -11,4 +11,35 @@ angular.module('postCtrl', ['postService'])
 			// bind all posts that come back to vm.posts 
 			vm.posts = data;
 		});
+		
+		// function to create post
+		vm.createPost = function() {
+			
+			// clear the message
+			vm.message = '';
+			
+			Post.create(vm.postData)
+				.success(function(data) {
+				
+				// clear the form 
+				vm.postData = {};
+				vm.message = data.message;
+				
+				// reload page to update view
+				$route.reload();
+			});
+		};
+	
+		// function to show likes increasing without reload
+		vm.incrementLikes = function(post) {
+			post.likes += 1;
+		};
+		// function to like a post
+		vm.likePost = function(id) {	
+			
+			Post.like(id)
+				.success(function(data) {
+					vm.message = data.message;
+			});
+		};
 	});
